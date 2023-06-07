7cbe90dde5317a9b90e53162aff7a93a38bc50d0
Maxim <fedyk.m@isitlab.com>
2023-06-07 14:28:28 +0300

    Initial Commit

---
+++ MyFirstCalculator/MyFirstCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstCalculatorApp.swift
+//  MyFirstCalculator
+//
+//  Created by Fedyk Maksym on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
